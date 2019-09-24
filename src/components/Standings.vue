cb79a12e149be7261caac35e50dd63988cd5bae0
Phillip Horrex <pjhorrex@gmail.com>
2019-09-24 00:54:25 -0400

    Moving things to variable partial

--- src/components/Standings.vue
+++ src/components/Standings.vue
@@ -11,7 +11,7 @@
         </div>
       </div>
       <div class="team-results-body">
-        <div class="team-results-row">
+        <div class="team-results-row player-header">
           <div class="team-results-row-name">{{ standing.name }}</div>
           <div class="team-results-row-result">{{ standing.win }}</div>
           <div class="team-results-row-result">{{ standing.loss }}</div>
@@ -57,6 +57,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '../sass/variables.scss';
+
   #standings {
     display: flex;
     flex-wrap: wrap;
@@ -74,7 +76,7 @@ export default {
   .team-results-table {
     margin: 10px;
     padding: 15px;
-    background-color: #eee;
+    background-color: $light;
     border-radius: 15px;
     flex: 0 1 320px;
   }
@@ -94,16 +96,19 @@ export default {
     font-weight: bold;
     width: 100%;
     padding: 5px 0;
-    border-radius: 8px;
+
+    &.player-header {
+      border-radius: 8px;
+    }
   }
 
   .team-results-row:not(.nfl-team-row):not(.header-row) {
-    background-color: #5a6673;
-    color: #f2f2f2;
+    background-color: $dark;
+    color: $light;
   }
 
   .nfl-team-row:not(:last-child) {
-    border-bottom: 2px #cacaca dotted;
+    border-bottom: 2px $medium-light dotted;
   }
 
   .team-results-row-name,
@@ -131,7 +136,7 @@ export default {
   }
 
   .nfl-team-row {
-    color: #9d9d9d;
+    color: $medium;
   }
 
   .nfl-team > span {
@@ -142,171 +147,18 @@ export default {
 
   .team-draft-picks {
     padding-top: 5px;
-    color: #9d9d9d;
+    color: $medium;
   }
 
   .team-draft-picks-pick:not(:last-child)::after {
-    content: ",\00a0";
+    content: ",#{map-get($symbols, "nbsp")}";
   }
 
-  .nfl-team > span#PHI {
-    background-color: #004C54;
-    color: #A5ACAF;
-  }
-
-  .nfl-team > span#NE {
-    background-color: #C60C30;
-    color: #002244;
-  }
-
-  .nfl-team > span#ARI {
-    background-color: #97233F;
-    color: #FFB612;
-  }
-
-  .nfl-team > span#BUF {
-    background-color: #00338D;
-    color: #C60C30;
-  }
-
-  .nfl-team > span#CIN {
-    background-color: #FB4F14;
-    color: #000000;
-  }
-
-  .nfl-team > span#DEN {
-    background-color: #FB4F14;
-    color: #002244;
-  }
-
-  .nfl-team > span#HOU {
-    background-color: #03202F;
-    color: #A71930;
-  }
-
-  .nfl-team > span#KC {
-    background-color: #E31837;
-    color: #FFFFFF;
-  }
-
-  .nfl-team > span#MIN {
-    background-color: #4F2683;
-    color: #FFC62F;
-  }
-
-  .nfl-team > span#NYG {
-    background-color: #0B2265;
-    color: #A71930;
-  }
-
-  .nfl-team > span#SF {
-    background-color: #B3995D;
-    color: #AA0000;
-  }
-
-  .nfl-team > span#TEN {
-    background-color: #418FDE;
-    color: #0C2340;
-  }
-
-  .nfl-team > span#ATL {
-    background-color: #A71930;
-    color: #000000;
-  }
-
-  .nfl-team > span#CAR {
-    background-color: #0085CA;
-    color: #101820;
-  }
-
-  .nfl-team > span#CLE {
-    background-color: #311D00;
-    color: #FF3C00;
-  }
-
-  .nfl-team > span#DET {
-    background-color: #0076B6;
-    color: #B0B7BC;
-  }
-
-  .nfl-team > span#IND {
-    background-color: #002C5F;
-    color: #FFFFFF;
-  }
-
-  .nfl-team > span#LAR {
-    background-color: #002244;
-    color: #B3995D;
-  }
-
-  .nfl-team > span#NYJ {
-    background-color: #125740;
-    color: #FFFFFF;
-  }
-
-  .nfl-team > span#PIT {
-    background-color: #101820;
-    color: #FFB612;
-  }
-
-  .nfl-team > span#SEA {
-    background-color: #002244;
-    color: #69BE28;
-  }
-
-  .nfl-team > span#WAS {
-    background-color: #773141;
-    color: #FFB612;
-  }
-
-  .nfl-team > span#BAL {
-    background-color: #241773;
-    color: #9E7C0C;
-  }
-
-  .nfl-team > span#CHI {
-    background-color: #0B162A;
-    color: #C83803;
-  }
-
-  .nfl-team > span#DAL {
-    background-color: #869397;
-    color: #041E42;
-  }
-
-  .nfl-team > span#GB {
-    background-color: #203731;
-    color: #FFB612;
-  }
-
-  .nfl-team > span#JAX {
-    background-color: #006778;
-    color: #D7A22A;
-  }
-
-  .nfl-team > span#MIA {
-    background-color: #008E97;
-    color: #FC4C02;
-  }
-
-  .nfl-team > span#NO {
-    background-color: #D3BC8D;
-    color: #101820;
-  }
-
-  .nfl-team > span#OAK {
-    background-color: #C4C9CC;
-    color: #000000;
-  }
-
-  .nfl-team > span#LAC {
-    background-color: #0080C6;
-    color: #FFC20E;
-  }
-
-  .nfl-team > span#TB {
-    background-color: #D50A0A;
-    color: #FF7900;
+  @each $team, $primary, $secondary in $teams {
+    .nfl-team > span##{$team} {
+      background-color: $primary;
+      color: $secondary;
+    }
   }
 
 </style>
